8798e43c4630690c82d71a6f255973f796f67a19
Perry <grperry@americas.corpdir.net>
2018-05-16 23:07:47 -0700

    updated git commit time in globalData.js file

--- globalData.js
+++ globalData.js
@@ -1,1 +1,1 @@
-{"branches":{},"jobs":{},"latest":{"develop":{"number":110,"gitCommitTime":"2018.05.09.06.12"}}}
+{"branches":{},"jobs":{},"latest":{"develop":{"number":110,"gitCommitTime":"2018-05-16T22:57:45-0700"}}}
