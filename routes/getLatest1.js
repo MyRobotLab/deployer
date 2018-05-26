78af03459ff51e81824a459de78a2cdefb0b49db
supertick <grog@myrobotlab.org>
2018-05-25 19:49:31 -0700

    status image endpoint

---
+++ routes/getLatest1.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  // res.send(jobs);
+
+  var branch = req.param("branch");
+  if (branch == null){
+    branch = "develop";
+  }
+  var url = "/builds/origin/" + branch + "/" + globalData.latest[branch].jobName + "/" + globalData.latest[branch].build.name + "/target/myrobotlab.jar";
+  console.log(url);
+  // http://build.myrobotlab.org:8888/builds/origin/develop/myrobotlab-grog-x86.64.linux-work-e/79/target/
+  res.redirect(url);
+  // res.send(url);
+});
+
+module.exports = router;
---
+++ routes/getLatestStatus.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  // res.send(jobs);
+
+  var branch = req.param("branch");
+  if (branch == null){
+    branch = "develop";
+  } 
+
+  var status = "error";
+  if (globalData.latest[branch].build.tests.errors == 0){
+    status = "success";
+  }
+
+  res.send("http://build.myrobotlab.org:8888/images/icon_"+status+"_sml.gif");
+});
+
+module.exports = router;
